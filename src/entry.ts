6f6aeda804017c28d1dcb426f068faf138459963
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-17 21:58:38 -0500

    feat(TvCard): add install method for plugin integration

--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,14 @@
 import TvCard from './components/TvCard.vue'
 
+(TvCard as any).install = (app: any) => {
+  app.component('TvCard', TvCard)
+};
+
+export const TvCardPlugin = {
+  install(app: any) {
+    app.component('TvCard', TvCard)
+  }
+}
+
 export { TvCard }
 export default TvCard
