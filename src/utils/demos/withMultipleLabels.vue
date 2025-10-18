196f4473c1da0ebfbd906df43a83c6d052553211
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-17 22:00:10 -0500

    fix(demo): update TvCard import to named exports in multiple components

--- src/utils/demos/withMultipleLabels.vue
+++ src/utils/demos/withMultipleLabels.vue
@@ -8,7 +8,7 @@
 
 <script setup>
 import { ref } from 'vue';
-import TvCard from '@todovue/tv-card';
+import { TvCard } from '@todovue/tv-card';
 
 const configCard = ref({
   title: 'Write JavaScript',
